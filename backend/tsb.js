8f9b4ae4e874a10d7bc486648a1601bf7288b169
Daniel-Elia Feist-Alexandrov <defa01@sccs.swarthmore.edu>
2014-12-02 20:35:16 -0500

    added https support

--- backend/tsb.js
+++ backend/tsb.js
@@ -3,11 +3,21 @@
  *  Version 0.0.1 - November 9, 2014
  */
 
-var express  = require('express');
-var connect  = require('connect');
-var app      = express();
-var port     = process.env.PORT || 8080;
-var db 		 = require('./modules/db/db.js');
+var express   = require('express');
+var connect   = require('connect');
+var app       = express();
+
+var port      = process.env.PORT || 8080;
+var httpsPort = 8443;
+var db 		  = require('./modules/db/db.js');
+var fs 		  = require('fs');
+
+// SSL configuration
+//var privateKey = fs.readFileSync('etc/ssl/private/server.key', 'utf8');
+//var certificate = fs.readFileSync('etc/ssl/private/server.crt', 'utf8');
+var http = require('http');
+var https = require('https');
+//var credentials = {key: privateKey, cert: certificate};
 
 // Configuration
 app.use(express.static(__dirname + '/public'));
@@ -17,7 +27,16 @@ app.use(connect.urlencoded());
 
 // Routes
 require('./routes.js')(app);
-app.listen(port);
+//app.listen(port);
+
+
+// Set up the servers
+var httpServer = http.createServer(app);
+//var httpsServer = https.createServer(credentials, app);
+
+// Listen on the respective ports
+httpServer.listen(port);
+//httpsServer.listen(httpsPort);
 
 
 console.log('TSB is running on port ' + port);
