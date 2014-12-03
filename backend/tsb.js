6eb9bd5370e486f3bf64c60dace2611b8789fe70
Daniel-Elia Feist-Alexandrov <defa01@sccs.swarthmore.edu>
2014-12-02 21:01:11 -0500

    added https flag to run with https enabled

--- backend/tsb.js
+++ backend/tsb.js
@@ -3,6 +3,13 @@
  *  Version 0.0.1 - November 9, 2014
  */
 
+
+// ARGS : node tsb.js <https>
+// If https is supplied, the server will also listen on port 8443 
+// for HTTPS connections
+var runWithHttps = process.argv[2] == 'https';
+
+
 var express   = require('express');
 var connect   = require('connect');
 var app       = express();
@@ -11,13 +18,16 @@ var port      = process.env.PORT || 8080;
 var httpsPort = 8443;
 var db 		  = require('./modules/db/db.js');
 var fs 		  = require('fs');
+var http 	  = require('http');
+var https 	  = require('https');
 
 // SSL configuration
-//var privateKey = fs.readFileSync('etc/ssl/private/server.key', 'utf8');
-//var certificate = fs.readFileSync('etc/ssl/private/server.crt', 'utf8');
-var http = require('http');
-var https = require('https');
-//var credentials = {key: privateKey, cert: certificate};
+
+if (runWithHttps) {
+	var privateKey = fs.readFileSync('etc/ssl/private/server.key', 'utf8');
+	var certificate = fs.readFileSync('etc/ssl/private/server.crt', 'utf8');
+	var credentials = {key: privateKey, cert: certificate};
+}
 
 // Configuration
 app.use(express.static(__dirname + '/public'));
@@ -32,11 +42,15 @@ require('./routes.js')(app);
 
 // Set up the servers
 var httpServer = http.createServer(app);
-//var httpsServer = https.createServer(credentials, app);
-
-// Listen on the respective ports
 httpServer.listen(port);
-//httpsServer.listen(httpsPort);
+
+if (runWithHttps) {
+	var httpsServer = https.createServer(credentials, app);
+	httpsServer.listen(httpsPort);
+}
 
 
 console.log('TSB is running on port ' + port);
+if (runWithHttps) {
+	console.log('TSB is also running on port ' + httpsPort);
+}
