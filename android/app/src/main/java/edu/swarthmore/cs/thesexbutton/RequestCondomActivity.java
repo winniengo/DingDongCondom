a72d6ac33fdd901fb51d92857618b6f5090ca516
Awjin <awjinahn@gmail.com>
2014-12-03 16:36:03 -0500

    Removed mail stuff. Fixed button click rate limiting on request condom activity.

--- android/app/src/main/java/edu/swarthmore/cs/thesexbutton/RequestCondomActivity.java
+++ android/app/src/main/java/edu/swarthmore/cs/thesexbutton/RequestCondomActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,16 +25,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class RequestCondomActivity extends Activity implements AdapterView.OnItemSelectedListener {
     String mSessionToken = null;
     String mDormName = null;
-    String mDormNumberString = null;
     String mDeliveryType = null;
-    Button mRequestButton;
     EditText mDormNumber;
+    String mDormNumberString = null;
+    boolean mDeliveryTypeFilled = false;
+    boolean mDormFilled = false;
+    Button mRequestButton;
     List<NameValuePair> mParams;
     SharedPreferences mSharedPreferences;
 
@@ -44,14 +47,14 @@ public class RequestCondomActivity extends Activity implements AdapterView.OnIte
         mSharedPreferences = getSharedPreferences("SharedPreferences", MODE_PRIVATE);
         mSessionToken = mSharedPreferences.getString("session_token", null);
 
-        // allow networking in the main thread
+        // Allow networking in the main thread
         if (android.os.Build.VERSION.SDK_INT > 9)
         {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
 
-        // generate dorm name spinner
+        // Generate dorm name spinner
         Spinner spinner = (Spinner) findViewById(R.id.dorms_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.dorms_array, android.R.layout.simple_spinner_dropdown_item);
@@ -59,14 +62,35 @@ public class RequestCondomActivity extends Activity implements AdapterView.OnIte
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
+        // Dorm number field
         mDormNumber = (EditText)findViewById(R.id.dorm_number);
+        mDormNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // do nothing
+            }
 
-        // submit request
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mDormNumberString = s.toString();
+                mDormFilled = true;
+                if (mDeliveryTypeFilled) {
+                    mRequestButton.setEnabled(true);
+                    mRequestButton.setBackgroundColor(getResources().getColor(R.color.green));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // do nothing
+            }
+        });
+
         mRequestButton = (Button) findViewById(R.id.request_condom_button);
         mRequestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mDormNumberString = mDormNumber.getText().toString();
+                mRequestButton.setEnabled(false);
 
                 mParams = new ArrayList<NameValuePair>();
                 mParams.add(new BasicNameValuePair("session_token", mSessionToken));
@@ -77,14 +101,12 @@ public class RequestCondomActivity extends Activity implements AdapterView.OnIte
                 ServerRequest serverRequest = new ServerRequest();
                 JSONObject json = serverRequest.getJSON("http://tsb.sccs.swarthmore.edu:8080/api/delivery/request", mParams);
 
-                if (json != null){
-                    try{
+                if (json != null) {
+                    try {
                         String jsonString = json.getString("response");
                         String orderNumber = json.getString("order_number");
 
                         Toast.makeText(getApplication(),jsonString,Toast.LENGTH_LONG).show();
-                        // send notification email
-                        generateDeliveryEmail(mDormName, mDormNumberString, mDeliveryType, orderNumber);
                         Log.d("Order Requested:", orderNumber);
 
                         // call Delivery Status Activity
@@ -94,7 +116,7 @@ public class RequestCondomActivity extends Activity implements AdapterView.OnIte
                         i.putExtras(b);
                         startActivity(i);
                         finish();
-                    }catch (JSONException e) {
+                    } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
@@ -103,38 +125,25 @@ public class RequestCondomActivity extends Activity implements AdapterView.OnIte
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.request_condom, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return (id == R.id.action_settings || super.onOptionsItemSelected(item));
-    }
-
     // Radio Button method
     public void onRadioButtonClicked(View view) {
-        boolean checked = ((RadioButton) view).isChecked();
+        mDeliveryTypeFilled = ((RadioButton) view).isChecked();
 
-        // check which radio button was clicked
+        // Check which radio button was clicked
         switch(view.getId()) {
             case R.id.request_condom_delivery_type_radio_to_lounge:
-                if (checked)
-                    // Lounge Delivery
+                if (mDeliveryTypeFilled)
                     mDeliveryType = "Lounge";
                 break;
             case R.id.request_condom_delivery_type_radio_to_room:
-                if (checked)
-                    // Room delivery
+                if (mDeliveryTypeFilled)
                     mDeliveryType = "Room";
                 break;
+        }
+
+        if(mDormFilled) {
+            mRequestButton.setEnabled(true);
+            mRequestButton.setBackgroundColor(getResources().getColor(R.color.green));
         }
     }
 
@@ -148,52 +157,21 @@ public class RequestCondomActivity extends Activity implements AdapterView.OnIte
         mDormName = null;
     }
 
-    // Mail method
-    private void generateDeliveryEmail(String dorm_name, String dorm_number, String delivery_type, String order_number) {
-        Mail m = new Mail("tsbdaemon@gmail.com", "9854<>47f?8l05X");
 
-        String[] recipients = {"tsbdaemon@gmail.com"};
-        String body = "Condom Delivery Request\n\n";
-
-        body += "Time and Date submitted:\t\t";
-        body += new Date().toString();
-
-        body += "Order number:\t\t";
-        body += order_number;
-
-        /*
-        body += "\nDevice number:\t\t\t\t\t";
-        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        String id = telephonyManager.getDeviceId();
-        body += id;
-        */
-
-        body += "\n\nDorm Name:\t\t\t\t\t";
-        body += dorm_name;
-
-        //get the delivery choice
-        body += "\n\nDelivery Type:";
-        body += "\t\t\t\t\t" + delivery_type;
-
-        // get the dorm room number
-        body += "\n\nFloor/Room Number:\t\t\t";
-        body += dorm_number;
-
-        m.set_to(recipients);
-        m.set_from("tsbdaemon@gmail.com");
-        m.set_subject("Delivery Request from user");
-        m.set_body(body);
-
-        try {
-            if(m.send()) {
-                Toast.makeText(RequestCondomActivity.this, "Request was sent successfully", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(RequestCondomActivity.this, "Request failed. Please try again.", Toast.LENGTH_LONG).show();
-            }
-        } catch(Exception e) {
-            //Toast.makeText(MailApp.this, "There was a problem sending the email.", Toast.LENGTH_LONG).show();
-            Log.e("MailApp", "Could not send email", e);
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.request_condom, menu);
+        return true;
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return (id == R.id.action_settings || super.onOptionsItemSelected(item));
+    }
 }
