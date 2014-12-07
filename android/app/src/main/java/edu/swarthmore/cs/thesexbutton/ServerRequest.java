ed4c66038039366a7ce3bdbc8ce2e6b30f5e78cd
Daniel-Elia Feist-Alexandrov <defa01@sccs.swarthmore.edu>
2014-12-06 23:05:04 -0500

    prepared serverRequest() for HTTPS switch

--- android/app/src/main/java/edu/swarthmore/cs/thesexbutton/ServerRequest.java
+++ android/app/src/main/java/edu/swarthmore/cs/thesexbutton/ServerRequest.java
@@ -33,6 +33,9 @@ import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+//our custom SSL enabled HTTP client
+import edu.swarthmore.cs.thesexbutton.tsbHTTPSClient;
+
 public class ServerRequest {
     static InputStream mInputStream = null;
     static JSONObject mJsonObject = null;
@@ -112,17 +115,4 @@ public class ServerRequest {
         }
     }
 
-    private HttpClient createHttpsClient() {
-        HttpParams params = new BasicHttpParams();
-        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-        HttpProtocolParams.setContentCharset(params, HTTP.DEFAULT_CONTENT_CHARSET);
-        HttpProtocolParams.setUseExpectContinue(params, true);
-
-        SchemeRegistry schReg = new SchemeRegistry();
-        schReg.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-        schReg.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
-        ClientConnectionManager conMgr = new ThreadSafeClientConnManager(params, schReg);
-
-        return new DefaultHttpClient(conMgr, params);
-    }
 }
